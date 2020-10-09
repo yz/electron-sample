7659101de6516f64e307393138b7258b6ba75e38
yz <yunzhang1@gmail.com>
2020-10-09 17:06:14 -0400

    auto update bug fixed.
    checkForUpdatesAndNotify() on ready fixed the issue.

--- main.js
+++ main.js
@@ -15,13 +15,11 @@ function createWindow () {
   mainWindow.on('closed', function () {
     mainWindow = null;
   });
-  mainWindow.once('ready-to-show', () => {
-    autoUpdater.checkForUpdatesAndNotify();
-  });
 }
 
 app.on('ready', () => {
   createWindow();
+  autoUpdater.checkForUpdatesAndNotify();
 });
 
 app.on('window-all-closed', function () {
